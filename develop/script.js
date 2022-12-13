c31aa12ed8d45597226f3378a49420f843e1f185
jshvaron <jshva@icloud.com>
2022-12-12 21:26:43 -0600

    adds initial function for char length and prompts if length is invalid

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,32 @@
-// Arrays for Character selection
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+// Var Arrays for Character selection
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var number = ["1234567890"];
+var specialCharacters = ["!@#$%^&*?=+"];
+var fullArray = []
+
+// First prompt selects length and alerts if invalid
+function generatePassword(){
+  var validInput = true
+  var psw =  prompt("Enter number of characters needed. Choose a length of 8-128: ")
+  if (psw < 8 || psw > 128){
+    alert("Your character length of: " + psw + " is invalid. Please select a character length of 8-128" )
+    return;
+  }
+}
+
+
+
 
 
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -17,7 +35,8 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
